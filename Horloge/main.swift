92885757418b39483d93e95015355122c2bb06a4
Lucas-Arkana <lucas-arkana@MacBook-Air-de-Lucas-Arkana.local>
2015-02-24 14:36:55 +0100

    Initial Commit

---
+++ Horloge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Horloge
+//
+//  Created by Lucas-Arkana on 24/02/2015.
+//  Copyright (c) 2015 Lucas Arkana. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
